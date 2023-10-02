9a60bda169cd14cf27b8201f0108140736805d23
JODI ADRIAN <18102127@ittelkom-pwt.ac.id>
2023-10-02 22:54:01 +0700

    update first week

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -10,6 +10,7 @@
 // console.log(jenis_mobil);
 
 
+
 // array js
 
 // let warna = ['hijau','kuning','putih'];
@@ -24,89 +25,101 @@
 // ]
 // console.table(myBio)
 
-// JSON
-let dummy_data = JSON.parse(localStorage.getItem('projects'))
-            console.log(dummy_data)
-            function save() 
-            {
-                let projects = localStorage.getItem('projects');
+// // JSON
+// let dummy_data = JSON.parse(localStorage.getItem('projects'))
+//             console.log(dummy_data)
+//             function save() 
+//             {
+//                 let projects = localStorage.getItem('projects');
 
-                const form = document.querySelector('form');
-                const data = Object.fromEntries(new FormData(form).entries());
+//                 const form = document.querySelector('form');
+//                 const data = Object.fromEntries(new FormData(form).entries());
                 
-                if (projects == null) {
-                    localStorage.setItem('projects', JSON.stringify([]))
-                    save()
-                }
+//                 if (projects == null) {
+//                     localStorage.setItem('projects', JSON.stringify([]))
+//                     save()
+//                 }
                 
-                projects = JSON.parse(projects)
+//                 projects = JSON.parse(projects)
                 
-                projects.push(data)
+//                 projects.push(data)
 
-                localStorage.setItem('projects', JSON.stringify(projects))
-            }
-
-
-// const data = []
-
-// function submitData(event) {
-//   event.preventDefault()
-
-//   let nama = document.getElementById("input-blog-nama").value
-//   let start_date = document.getElementById("input-blog-start").value
-//   let end_date = document.getElementById("input-blog-end").value
-//   let description = document.getElementById("input-blog-description").value
-//   let image = document.getElementById("input-blog-image").files
-
-//   image = URL.createObjectURL(image[0])
-
-//   const obj = {
-//     nama,
-//     start_date,
-//     end_date,
-//     description,
-//     image,
-//     postedAt: new Date(),
-//     author: "jODI ADRIAN"
-//   }
-
-//   data.push(obj)
-//   renderBlog()
-// }
-
-// function renderBlog() {
-//   document.getElementById("contents").innerHTML = ""
-
-//   for(let i = 0; i < data.length; i++) {
-//     document.getElementById("contents").innerHTML += `<div class="blog-list-item">
-//     <div class="blog-image">
-//       <img src="${data[i].image}" alt="" />
-//     </div>
-//     <div class="blog-content">
-//       <div class="btn-group">
-//         <button class="btn-edit">Edit Post</button>
-//         <button class="btn-post">Post Blog</button>
-//       </div>
-//       <h1>
-//         <a href="blog-detail.html" target="_blank"
-//           >${data[i].title}</a
-//         >
-//       </h1>
-//       <div class="detail-blog-content">
-//         ${data[i].postedAt} | ${data[i].author}
-//       </div>
-//       <p>
-//         ${data[i].content}
-//       </p>
-
-//       <div style="float:right; margin: 10px">
-//         <p style="font-size: 15px; color:grey">1 minutes ago</p>
-//       </div>
-//     </div>
-//   </div>`
-//   }
-// }
+//                 localStorage.setItem('projects', JSON.stringify(projects))
+//             }
 
 
 
 
+const data = []
+
+function submitData(event) {
+    event.preventDefault()
+
+    let nama = document
+        .getElementById("project_name")
+        .value
+    let start_date = document
+        .getElementById("start_date")
+        .value
+    let end_date = document
+        .getElementById("end_date")
+        .value
+    let description = document
+        .getElementById("description")
+        .value
+    let technologies = [];
+    document
+        .querySelectorAll('[name="technologies"]:checked')
+        .forEach((checkbox) => {
+            technologies.push(checkbox.value);
+        });
+    let image = document
+        .getElementById("input-blog-image")
+        .files
+
+    image = URL.createObjectURL(image[0])
+
+    const obj = {
+        nama,
+        start_date,
+        end_date,
+        description,
+        technologies,
+        image,
+        author: "jODI ADRIAN"
+    }
+
+    data.push(obj)
+    renderBlog()
+}
+
+function renderBlog() {
+    document
+        .getElementById("contents")
+        .innerHTML = ""
+
+    for (let i = 0; i < data.length; i++) {
+        document
+            .getElementById("contents")
+            .innerHTML += `<div class="div-card">
+    <img src="${data[i].image}" alt="" class="img-card"/>
+    <h1 class="h1-card">
+    ${data[i].nama}
+    </h1>
+    <p class="p-card">
+    ${data[i].start_date} - ${data[i].end_date}</p>
+
+    <table class="table-card">
+        <tr>
+            <th class="th-card">Deskripsi</th>
+            <th class="th-card">Technology</th>
+        </tr>
+        <tr>
+            <td class="td-card">
+            ${data[i].description}</td>
+            <td class="td-card">${data[i].technologies}</td>
+        </tr>
+    </table>
+</div>`
+    }
+}

583178d10ef4a698aabd11a2c8071c262c6162d7
JODI ADRIAN <18102127@ittelkom-pwt.ac.id>
2023-10-06 23:24:47 +0700

    count month and filtering

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,55 +1,3 @@
-// objek js
-
-// let jenis_mobil = {
-//     suv: "crv",
-//     sedan: "civic",
-//     crossover: "yaris"  
-
-// }
-
-// console.log(jenis_mobil);
-
-
-
-// array js
-
-// let warna = ['hijau','kuning','putih'];
-// warna[3] = 'merah';
-
-// console.log(warna[1])
-
-// array object js
-
-// const myBio = ["Rian", 23, true, 
-//     {id : 1, name: "laki-laki"}
-// ]
-// console.table(myBio)
-
-// // JSON
-// let dummy_data = JSON.parse(localStorage.getItem('projects'))
-//             console.log(dummy_data)
-//             function save() 
-//             {
-//                 let projects = localStorage.getItem('projects');
-
-//                 const form = document.querySelector('form');
-//                 const data = Object.fromEntries(new FormData(form).entries());
-                
-//                 if (projects == null) {
-//                     localStorage.setItem('projects', JSON.stringify([]))
-//                     save()
-//                 }
-                
-//                 projects = JSON.parse(projects)
-                
-//                 projects.push(data)
-
-//                 localStorage.setItem('projects', JSON.stringify(projects))
-//             }
-
-
-
-
 const data = []
 
 function submitData(event) {
@@ -76,6 +24,7 @@ function submitData(event) {
     let image = document
         .getElementById("input-blog-image")
         .files
+    let project_duration = countProjectDuration(); // 1 Bulan 2 Hari
 
     image = URL.createObjectURL(image[0])
 
@@ -86,7 +35,8 @@ function submitData(event) {
         description,
         technologies,
         image,
-        author: "jODI ADRIAN"
+        author: "jODI ADRIAN",
+        project_duration,
     }
 
     data.push(obj)
@@ -102,12 +52,14 @@ function renderBlog() {
         document
             .getElementById("contents")
             .innerHTML += `<div class="div-card">
-    <img src="${data[i].image}" alt="" class="img-card"/>
+    <img src="${data[i]
+            .image}" alt="" class="img-card"/>
     <h1 class="h1-card">
-    ${data[i].nama}
+    ${data[i]
+            .nama}
     </h1>
     <p class="p-card">
-    ${data[i].start_date} - ${data[i].end_date}</p>
+    ${data[i].project_duration}</p>
 
     <table class="table-card">
         <tr>
@@ -116,10 +68,81 @@ function renderBlog() {
         </tr>
         <tr>
             <td class="td-card">
-            ${data[i].description}</td>
-            <td class="td-card">${data[i].technologies}</td>
+            ${data[i]
+            .description}</td>
+            <td class="td-card">${data[i]
+            .technologies}</td>
         </tr>
     </table>
 </div>`
     }
+}
+
+function countProjectDuration() {
+    // get value from input
+	let start_date = document.getElementById("start_date").value;
+	let end_date = document.getElementById("end_date").value;
+
+    // convert string to date
+	start_date = new Date(start_date);
+	end_date = new Date(end_date);
+
+	// Calculate the project duration in days by subtracting the start_date from the end_date.
+    let project_duration = end_date - start_date;
+
+    // Convert the duration from milliseconds to days by dividing it by the number of milliseconds in a day.
+    project_duration = project_duration / (1000 * 60 * 60 * 24);
+
+    // Round the calculated duration to the nearest whole number of days.
+    project_duration = Math.round(project_duration);
+
+    // Calculate the number of months in the project duration by dividing it by an approximate number of days in a month (30 days).
+    month = Math.floor(project_duration / 30);
+
+    // Calculate the remaining days after calculating the number of months.
+    day = project_duration % 30;
+
+
+    // return string project duration (month and day)
+	return `Durasi: ${month} Bulan ${day} Hari`; // 1 Bulan 2 Hari
+}
+
+
+function filterProjectBasedOnTechnology() {
+    // get value from select option
+	let technology = document.getElementById("filter_technology").value;
+
+    // if value is all, render all data
+	if (technology == "all") {
+		renderBlog();
+	} else {
+		document.getElementById("contents").innerHTML = "";
+
+		for (let i = 0; i < data.length; i++) {
+			if (data[i].technologies.includes(technology)) {
+				document.getElementById(
+					"contents"
+				).innerHTML += `<div class="div-card">
+                    <img src="${data[i].image}" alt="" class="img-card"/>
+                    <h1 class="h1-card">
+                    ${data[i].nama}
+                    </h1>
+                    <p class="p-card">
+                    ${data[i].project_duration}</p>
+
+                    <table class="table-card">
+                        <tr>
+                            <th class="th-card">Deskripsi</th>
+                            <th class="th-card">Technology</th>
+                        </tr>
+                        <tr>
+                            <td class="td-card">
+                            ${data[i].description}</td>
+                            <td class="td-card">${data[i].technologies}</td>
+                        </tr>
+                    </table>
+                </div>`;
+			}
+		}
+	}
 }
